6e963dca8916b5ed95c5e17581cb982ca59a7e63
Anna Letycia <anna.fernandes@ufu.br>
2022-12-07 20:17:14 -0300

    Create ex01.c

---
+++ Lab12 - Functions/ex01/ex01.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+void DesenhaLinha(int num)
+{   
+    for(int i; i<num; i++)
+    printf("==========\n");
+
+}
+
+int main()
+{
+    int num;
+
+    printf("Digite a quantidade de linhas a serem impressas: ");
+    scanf("%d", &num);
+    
+    DesenhaLinha(num);
+    
+    return 0;
+}
+
+/*
+=> Faça um procedimento chamado DesenhaLinha. Ele deve desenhar 
+uma linha na tela usando vários caracteres "=" (Ex: ========). 
+No programa principal execute várias chamadas a esse procedimento (use um loop).
+
+Ex: DesenhaLinha(3) tem como saída:
+    ========
+    ========
+    ======== 
+
+No programa principal, solicite ao usuário quantas vezes a linha deverá ser desenhada.
+
+-------------------------------------------------------------------------------
+Exemplo de Saída:
+
+Digite a quantidade de linhas a serem impressas: 3
+========
+========
+========
+-------------------------------------------------------------------------------
+*/
